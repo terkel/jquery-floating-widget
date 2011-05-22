55baa0d8086518057db296d5e9d29d2a36d3c6f6
Takeru Suzuki <terkeljp@gmail.com>
2011-05-22 18:21:08 +0900

    README

--- jquery.floating-widget.js
+++ jquery.floating-widget.js
@@ -1,5 +1,6 @@
 /*
  * jQuery Floating Widget plugin v0.9
+ * http://terkel.jp/archives/2011/05/jquery-floating-widget-plugin/
  *
  * Copyright (C) 2011 Takeru Suzuki (http://terkel.jp/)
  * Licensed under a Creative Commons Attribution 2.1 Japan
@@ -8,22 +9,22 @@
 (function ($) {
     $.fn.floatingWidget = function () {
         return this.each(function () {
-            var $this         = $(this),
-                $parent       = $this.offsetParent(),
-                $window       = $(window),
-                top           = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0)),
-                bottom        = $parent.offset().top + $parent.height() - $this.outerHeight(true),
-                floatingClass = 'floating',
-                bottomClass   = 'bottom';
+            var $this             = $(this),
+                $parent           = $this.offsetParent(),
+                $window           = $(window),
+                top               = $this.offset().top - parseFloat($this.css('marginTop').replace(/auto/, 0)),
+                bottom            = $parent.offset().top + $parent.height() - $this.outerHeight(true),
+                floatingClass     = 'floating',
+                pinnedBottomClass = 'pinned-bottom';
             if ($parent.height() > $this.outerHeight(true)) {
                 $window.scroll(function () {
                     var y = $window.scrollTop();
                     if (y > top) {
                         $this.addClass(floatingClass);
                         if (y > bottom) {
-                            $this.removeClass(floatingClass).addClass(bottomClass);
+                            $this.removeClass(floatingClass).addClass(pinnedBottomClass);
                         } else {
-                            $this.removeClass(bottomClass);
+                            $this.removeClass(pinnedBottomClass);
                         }
                     } else {
                         $this.removeClass(floatingClass);
